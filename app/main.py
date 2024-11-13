e90dc5abe1ee3d234c2ae6c07870dd4e0354c50b
Areej Naqvi <areej.faisal@conradlabs.com>
2024-11-13 16:24:27 +0500

    fix: fix CORS errors

--- app/main.py
+++ app/main.py
@@ -4,12 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Custom Canopy Mockup API")
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Update with your frontend URL
+    allow_origins=[
+        "http://localhost:3000",
+        "https://custom-canopy-chatbot-4ttha4kgf-team-alpha-8576f1e5.vercel.app",
+        "https://custom-canopy-fastapi-server-1f8879954a5f.herokuapp.com"  
+    ],
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"], 
+    allow_headers=["*"], 
 )
 
 app.include_router(router)
