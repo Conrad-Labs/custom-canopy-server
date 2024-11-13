70f053bfe89b7ca58a8b0e329df58c90cd95fd52
Areej Naqvi <areej.faisal@conradlabs.com>
2024-11-13 18:19:28 +0500

    fix: fix CORS middleware bug

--- app/main.py
+++ app/main.py
@@ -7,9 +7,9 @@ app = FastAPI(title="Custom Canopy Mockup API")
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origin_regex=r"https://custom-canopy-chatbot-[a-zA-Z0-9-]+\.vercel\.app",
     allow_origins=[
         "http://localhost:3000",
-        "https://custom-canopy-chatbot-4ttha4kgf-team-alpha-8576f1e5.vercel.app",
         "https://custom-canopy-fastapi-server-1f8879954a5f.herokuapp.com"  
     ],
     allow_credentials=True,
