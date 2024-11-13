b22e8c877390e0a1dc0e684c8de0bfab201e1abf
Areej Naqvi <areej.faisal@conradlabs.com>
2024-11-13 22:34:19 +0500

    fix: update allowed origins

--- app/main.py
+++ app/main.py
@@ -7,7 +7,11 @@ app = FastAPI(title="Custom Canopy Mockup API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://custom-canopy-chatbot-app-git-disable-form-team-alpha-8576f1e5.vercel.app", 
+        "https://localhost:3000",
+        "https://custom-canopy-fastapi-server-1f8879954a5f.herokuapp.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
