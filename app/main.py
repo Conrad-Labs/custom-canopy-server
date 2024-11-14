fcd964f80307aeef2dbac622a4ea6713c909937a
Areej Naqvi <areej.faisal@conradlabs.com>
2024-11-14 13:49:00 +0500

    fix: allow frontend url on origin

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
         "https://custom-canopy-chatbot-app-git-disable-form-team-alpha-8576f1e5.vercel.app", 
         "http://localhost:3000",
         "https://custom-canopy-fastapi-server-1f8879954a5f.herokuapp.com",
-        "https://custom-canopy-chatbot-ndyysqjtw-team-alpha-8576f1e5.vercel.app",
+        "https://custom-canopy-chatbot-cj0ienmgn-team-alpha-8576f1e5.vercel.app",
         "https://custom-canopy-chatbot-app.vercel.app",
     ],
     allow_credentials=True,
