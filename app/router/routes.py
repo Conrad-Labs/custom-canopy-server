4b6976a2f7276702cc265ba2530c4438cc48e8c9
Areej Naqvi <areej.faisal@conradlabs.com>
2025-03-27 13:26:30 +0500

    Revert "Feat: Allow different colors and text for each of the valences and peaks"

--- app/router/routes.py
+++ app/router/routes.py
@@ -1,176 +1,69 @@
+import io
 import json
-from typing import Optional
+import zipfile
 
 from fastapi import APIRouter, UploadFile, File, Form, HTTPException
-from app.schema import OverlayRequest, TentSides, ValencesText, AddOns, Table
-from app.services.image_processor import generate_mockups
-from app.constants import DEFAULT_FONT_COLOUR, DEFAULT_TENT_COLOR, DEFAULT_TEXT
-from dotenv import load_dotenv
+from fastapi.responses import StreamingResponse
+from app.schema import OverlayRequest
+from app.services.image_processor import apply_all_logos
+from app.constants import DEFAULT_TENT_COLOR, DEFAULT_TEXT, DEFAULT_FONT_COLOUR
 
-load_dotenv()
 router = APIRouter()
-
-
-def validate_color(color_str: str, optional=False):
-    if color_str:
-        color = json.loads(color_str)
-        if (
-            len(color) != 3 or not all(isinstance(c, int) for c in color)
-        ) and not optional:
-            raise ValueError(
-                "Color must be a list of three integers representing B, G, and R values."
-            )
-
-        return color
-    if optional:
-        return None
-    else:
-        raise ValueError(
-            "Color must be a list of three integers representing B, G, and R values."
-        )
-
 
 @router.post("/create-mockups", tags=["Mockup Creation"])
 async def create_mockups(
+    slope_color: str = Form(f'"{DEFAULT_TENT_COLOR}"'),
+    canopy_color: str = Form(f'"{DEFAULT_TENT_COLOR}"'),
+    walls_primary_color: str = Form(f'"{DEFAULT_TENT_COLOR}"'),
+    walls_secondary_color: str = Form(f'"{DEFAULT_TENT_COLOR}"'),
+    walls_tertiary_color: str = Form(f'"{DEFAULT_TENT_COLOR}"'),
+    text: str = Form(f'"{DEFAULT_TEXT}"'),
+    text_color: str = Form(f'"{DEFAULT_FONT_COLOUR}"'),
     logo: UploadFile = File(...),
-    peaks_front: str = Form(
-        f'"{DEFAULT_TENT_COLOR}"',
-        description="Front color for peaks. Must be a string representation of a list of three integers representing the BGR color value.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    peaks_left: Optional[str] = Form(
-        "",
-        description="Left color for peaks. Defaults to the same as peaks_front if not provided.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    peaks_right: Optional[str] = Form(
-        "",
-        description="Right color for peaks. Defaults to the same as peaks_front if not provided.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    peaks_back: Optional[str] = Form(
-        "",
-        description="Back color for peaks. Defaults to the same as peaks_front if not provided.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    valences_front: str = Form(
-        f'"{DEFAULT_TENT_COLOR}"',
-        description="Front color for valences. Must be a string representation of a list of three integers representing the BGR color value.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    valences_left: Optional[str] = Form(
-        "",
-        description="Left color for valences. Defaults to the same as valences_front if not provided.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    valences_right: Optional[str] = Form(
-        "",
-        description="Right color for valences. Defaults to the same as valences_front if not provided.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    valences_back: Optional[str] = Form(
-        "",
-        description="Back color for valences. Defaults to the same as valences_front if not provided.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    panels_back: Optional[str] = Form(
-        "",
-        description="Back color for panel. Must be a string representation of a list of three integers representing the BGR color value",
-    ),
-    panels_left: Optional[str] = Form(
-        "",
-        description="Left color for panel. Must be a string representation of a list of three integers representing the BGR color value",
-    ),
-    panels_right: Optional[str] = Form(
-        "",
-        description="Right color for panel. Must be a string representation of a list of three integers representing the BGR color value",
-    ),
-    text_color: str = Form(
-        f'"{DEFAULT_FONT_COLOUR}"',
-        description="Font color for the text. Must be a string representation of a list of three integers representing the BGR color value.",
-        example=f'"{DEFAULT_FONT_COLOUR}"',
-    ),
-    front_text: Optional[str] = Form(
-        "",
-        description="Text to be added to the front valence of the canopy.",
-        example=DEFAULT_TEXT,
-    ),
-    left_text: Optional[str] = Form(
-        "",
-        description="Text to be added to the left valence of the canopy.",
-        example=DEFAULT_TEXT,
-    ),
-    back_text: Optional[str] = Form(
-        "",
-        description="Text to be added to the back valence of the canopy.",
-        example=DEFAULT_TEXT,
-    ),
-    right_text: Optional[str] = Form(
-        "",
-        description="Text to be added to the right valence of the canopy.",
-        example=DEFAULT_TEXT,
-    ),
-    table_color: Optional[str] = Form(
-        "",
-        description="Color for the table. Must be a string representation of a list of three integers representing the BGR color value.",
-        example=f'"{DEFAULT_TENT_COLOR}"',
-    ),
-    output_dir: str = Form('"{DEFAULT_OUTPUT_DIR}"'),
+    patterned: str = Form('false')
 ):
     """
     Create mockups for canopy layouts with the provided logo, colour, and text, if any
     """
     try:
-        font_color = validate_color(text_color)
-        text = ValencesText(
-            front=front_text, left=left_text, back=back_text, right=right_text
-        )
-
-        peaks = TentSides(
-            front=validate_color(peaks_front),
-            left=validate_color(peaks_left, optional=True)
-            or validate_color(peaks_front),
-            right=validate_color(peaks_right, optional=True)
-            or validate_color(peaks_front),
-            back=validate_color(peaks_back, optional=True)
-            or validate_color(peaks_front),
-        )
-
-        valences = TentSides(
-            front=validate_color(valences_front),
-            left=validate_color(valences_left, optional=True)
-            or validate_color(valences_front),
-            right=validate_color(valences_right, optional=True)
-            or validate_color(valences_front),
-            back=validate_color(valences_back, optional=True)
-            or validate_color(valences_front),
-        )
-        panels = TentSides(
-            back=validate_color(panels_back, optional=True),
-            left=validate_color(panels_left, optional=True),
-            right=validate_color(panels_right, optional=True),
-        )
-
-        addons = (
-            AddOns(table=Table(sides=TentSides(front=validate_color(table_color))))
-            if table_color
-            else None
-        )
+        slope_color = json.loads(slope_color)
+        if len(slope_color) != 3 or not all(isinstance(c, int) for c in slope_color):
+            raise ValueError("Slope color must be a list of three integers representing B, G, and R values.")
+        
+        canopy_color = json.loads(canopy_color)
+        if len(canopy_color) != 3 or not all(isinstance(c, int) for c in canopy_color):
+            raise ValueError("Canopy color must be a list of three integers representing B, G, and R values.")
+        
+        walls_primary_color = json.loads(walls_primary_color)
+        if len(walls_primary_color) != 3 or not all(isinstance(c, int) for c in walls_primary_color):
+            raise ValueError("Walls' primary color must be a list of three integers representing B, G, and R values.")
+        
+        walls_secondary_color = json.loads(walls_secondary_color)
+        if len(walls_secondary_color) != 3 or not all(isinstance(c, int) for c in walls_secondary_color):
+            raise ValueError("Walls' secondary color must be a list of three integers representing B, G, and R values.")
+        
+        walls_tertiary_color = json.loads(walls_tertiary_color)
+        if len(walls_tertiary_color) != 3 or not all(isinstance(c, int) for c in walls_tertiary_color):
+            raise ValueError("Walls' tertiary color must be a list of three integers representing B, G, and R values.")
+        
+        font_color = json.loads(text_color)
+        if len(font_color) != 3 or not all(isinstance(c, int) for c in font_color):
+            raise ValueError("Font color must be a list of three integers representing B, G, and R values.")
 
         logo_content = await logo.read()
-        overlay_data = OverlayRequest(
-            peaks=peaks,
-            valences=valences,
-            panels=panels,
-            font_color=font_color,
-            text=text,
-            add_ons=addons,
-            output_dir=output_dir,
+        overlay_data = OverlayRequest(slope_color=slope_color, canopy_color=canopy_color, walls_primary_color=walls_primary_color, walls_secondary_color=walls_secondary_color, walls_tertiary_color=walls_tertiary_color, text=text, font_color=font_color, is_patterned=f"{patterned}")
+        
+        zip_buffer = io.BytesIO()
+        with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+            apply_all_logos(overlay_data, logo_content, zip_file)
+    
+        zip_buffer.seek(0)
+        return StreamingResponse(
+            zip_buffer,
+            media_type="application/zip",
+            headers={"Content-Disposition": "attachment; filename=mockups.zip"}
         )
 
-        mockups = generate_mockups(overlay_data, logo_content)
-        return mockups
-
     except Exception as e:
-        print(e)
         raise HTTPException(status_code=400, detail=str(e))
+    
